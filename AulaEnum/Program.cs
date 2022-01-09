10c7c8fd24c665beec438bbde78e424b685b5063
Wendel Soares <wendel.soares.dev@gmail.com>
2022-01-09 11:29:16 -0300

    Começando aula de enum

---
+++ AulaEnum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AulaEnum
+{
+    class Program
+    {
+        public void Main(String[] args)
+        {
+            Console.WriteLine("Bom dia!");
+        }
+    }
+}
