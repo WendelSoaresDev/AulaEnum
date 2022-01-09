9dfb529e10d02ce759a72b5235c22af72566cad3
Wendel Soares <wendel.soares.dev@gmail.com>
2022-01-09 11:57:02 -0300

    Conversão de enum p/ string e vice-versa

--- AulaEnum/Program.cs
+++ AulaEnum/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using AulaEnum.Entities;
+using AulaEnum.Entities.Enums; // para utilizar classes de outros namespaces
 namespace AulaEnum
 {
     class Program
     {
         public static void Main(String[] args)
         {
-            Order order = new Order();
+            Order order = new Order
+            { 
+                //Iniciando valores no objeto
+                Id = 1050,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+                
+            };
 
             Console.WriteLine(order);
+
+            //conversão de enumeração p/ string
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            //conversão de string p/ enumeração
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
         }
     }
 }
